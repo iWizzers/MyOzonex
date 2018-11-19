c1ccaa0110abd49d97bf5cec9b736f2f39d3add1
Mickael MERCIER <mickael.mercier@outlook.fr>
2018-11-19 13:37:48 +0100

    Fix fullscreen

--- MyOzonex/app/src/main/java/fr/ozonex/myozonex/MainActivity.java
+++ MyOzonex/app/src/main/java/fr/ozonex/myozonex/MainActivity.java
@@ -107,7 +107,6 @@ public class MainActivity extends AppCompatActivity
 
             decorView.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         } else {
             drawer.setFitsSystemWindows(false);
